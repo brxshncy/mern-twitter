07f31d05e0e60331ab621a34cfd2581cc801915b
brxshncy <ggkidsbruce619@gmail.com>
2024-04-19 15:21:32 +0800

    setup tailwind basic react rout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Newsfeed } from "./pages/Newsfeed";
+import { Register } from "./pages/Register";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Newsfeed />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+]);
+
+const App = () => {
+    return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ frontend/src/pages/Newsfeed.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Newsfeed = () => {
+    return <div>Newsfeed</div>;
+};
